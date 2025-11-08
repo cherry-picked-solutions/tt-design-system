9f5d386d9d9c971196add753c43d42a454352bb5
James Mejia <s.jamesmejia@gmail.com>
2025-11-08 10:45:14 -0600

    Create slot based header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,7 +7,27 @@ export class TTHeader extends LitElement {
     primaryNav: { type: Array },
   };
 
-  static styles = css``;
+  static styles = css`
+    :host {
+      container-type: inline-size;
+      container-name: header;
+      display: block;
+    }
+
+    /* @container (min-width: 768px) { */
+    @container (min-width: 500px) {
+      .tt-header {
+        display: grid;
+        align-items: center;
+        grid-template-columns: repeat(3, 1fr);
+        column-gap: var(--space-md);
+      }
+    }
+
+    .tt-header__center {
+      outline: 1px solid blue;
+    }
+  `;
 
   constructor() {
     super();
@@ -21,19 +41,10 @@ export class TTHeader extends LitElement {
 
   render() {
     return html`
-      <header>
-        <div slot="first">${this.title} ${this.logo}</div>
-        <div slot="second">
-          <nav>
-            <ul>
-              ${this.primaryNav.map(
-                (navLink) =>
-                  html`<li><a href=${navLink.href}>${navLink.label}</a></li>`,
-              )}
-            </ul>
-          </nav>
-        </div>
-        <div slot="third">CTA's</div>
+      <header class="tt-header">
+        <div class="tt-header__start"><slot name="start"></slot></div>
+        <div class="tt-header__center"><slot name="center"></slot></div>
+        <div class="tt-header__end"><slot name="end"></slot></div>
         <slot></slot>
       </header>
     `;
--- src/components/header/header.stories.js
+++ src/components/header/header.stories.js
@@ -1,17 +1,41 @@
 import { html, nothing } from "lit";
 import "./header";
+import "../button/button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
   title: "Example/Header",
   tags: ["autodocs"],
   component: "tt-header",
-  render: (args) => html`<tt-header> ${args.label} </tt-header>`,
+  render: (args) =>
+    html`<tt-header>
+      <div slot="start">
+        <h1>Hello world</h1>
+      </div>
+      <nav slot="center">
+        <ul className="nav">
+          <li>
+            <a href="#">Nav item 1</a>
+          </li>
+          <li>
+            <a href="#">Nav item 2</a>
+          </li>
+          <li>
+            <a href="#">Nav item 3</a>
+          </li>
+          <li>
+            <a href="#">Nav item 4</a>
+          </li>
+        </ul>
+      </nav>
+      <div slot="end">
+        <tt-button>Contact Us</tt-button>
+      </div>
+    </tt-header>`,
   args: {
     variant: "",
     label: "Frog",
   },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default = {};
