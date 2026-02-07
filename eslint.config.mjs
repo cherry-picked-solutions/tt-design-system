7c6fa0d80784950165fb47fae5c32d43cc9d2f2c
James Mejia <s.jamesmejia@gmail.com>
2026-02-07 12:55:36 -0600

    Update StorybookJS from v9 → 10. Closes #28.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import lit from "eslint-plugin-lit";
 
-export default defineConfig([
-  {
-    // ...litConfig["flat/recommended"],
-    files: ["src/components/**/*.{js,mjs,cjs}"],
-    plugins: { js, lit },
-    extends: ["js/recommended", "lit/recommended"],
-    languageOptions: { globals: globals.browser },
-  },
-  eslintConfigPrettier,
-]);
+export default defineConfig([{
+  // ...litConfig["flat/recommended"],
+  files: ["src/components/**/*.{js,mjs,cjs}"],
+  plugins: { js, lit },
+  extends: ["js/recommended", "lit/recommended"],
+  languageOptions: { globals: globals.browser },
+}, eslintConfigPrettier, ...storybook.configs["flat/recommended"]]);
