c2dc00710be924544542648b9fd0fe97ca9160b5
James Mejia <s.jamesmejia@gmail.com>
2026-03-26 21:32:45 -0500

    Scaffold component

---
+++ src/components/code-snippet/code-snippet.js
@@ -0,0 +1,23 @@
+import { LitElement, html } from "lit";
+
+/**
+ * @tag tt-code-snippet
+ * @summary A description.
+ *
+ * @cssprop [--tt-example=var(--token)]
+ *
+ */
+
+export class TTCodeSnippet extends LitElement {
+  static properties = {};
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html``;
+  }
+}
+
+customElements.define("tt-code-snippet", TTCodeSnippet);
